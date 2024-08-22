c8fd7b15f28a6939ec0ec616744afaca16f2ce97
Rebson <rebsonramalho@gmail.com>
2024-08-22 00:07:03 -0300

    deletando front

--- frontendEverton/scripts.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    const senha = document.querySelector('#senha').value;
-    const confirmarSenha = document.querySelector('#confirmarSenha').value;
-
-    if (senha !== confirmarSenha) {
-        event.preventDefault();
-        alert('As senhas não coincidem!');
-    }
-});
