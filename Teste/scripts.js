85e1d420c6ac4412a8cda9568dee9d622297ffc9
Rebson <rebsonramalho@gmail.com>
2024-08-22 19:55:05 -0300

    Jão é gay

--- Teste/scripts.js
+++ Teste/scripts.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
             dataNascimento: document.getElementById('dataNascimento').value,
             telefone: document.getElementById('telefone').value,
             email: document.getElementById('email').value,
-            
+
             estado: document.getElementById('estado').value,
             cidade: document.getElementById('cidade').value,
             cep: document.getElementById('cep').value,
@@ -45,6 +45,56 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => {
             console.error('Erro ao cadastrar servidor:', error);
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-alunos').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const alunos = {
+            
+            nome: document.getElementById('nome').value,
+            CPF: document.getElementById('cpf').value,
+            RG: document.getElementById('rg').value,
+            dataNascimento: document.getElementById('dataNascimento').value,
+            telefone: document.getElementById('telefone').value,
+            email: document.getElementById('email').value,
+            
+            estado: document.getElementById('estado').value,
+            cidade: document.getElementById('cidade').value,
+            cep: document.getElementById('cep').value,
+            bairro: document.getElementById('bairro').value,
+            rua: document.getElementById('rua').value,
+            numero: document.getElementById('numero').value,
+            complemento: document.getElementById('complemento').value,
+
+            nomeBanco: document.getElementById('nomeBanco').value,
+            numConta: document.getElementById('numConta').value,
+            agencia: document.getElementById('agencia').value,
+            operacao: document.getElementById('operacao').value
+                     };
+
+        fetch('http://localhost:8080/alunos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(alunos)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao cadastrar aluno: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert('aluno cadastrado com sucesso!');
+            console.log('Success:', data);
+        })
+        .catch(error => {
+            console.error('Erro ao cadastrar aluno:', error);
         });
     });
 });
