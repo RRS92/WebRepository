009f93138ff5aecfa3ea5cbe176f0bf7037d340b
Rebson <rebsonramalho@gmail.com>
2024-08-22 18:27:42 -0300

    teste 5

--- frontendEverton/scripts.js
+++ frontendEverton/scripts.js
@@ -1,36 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('form-servidor').addEventListener('submit', function(event) {
+document.querySelector('form').addEventListener('submit', function(event) {
+    const senha = document.querySelector('#senha').value;
+    const confirmarSenha = document.querySelector('#confirmarSenha').value;
+
+    if (senha !== confirmarSenha) {
         event.preventDefault();
-
-        const servidor = {
-            nome: document.getElementById('nome').value,
-            siape: document.getElementById('siape').value,
-            CPF: document.getElementById('cpf').value,  // Atualizado para maiúsculas
-            RG: document.getElementById('rg').value,   // Atualizado para maiúsculas
-            idade: parseInt(document.getElementById('idade').value, 10),  // Assegure-se de que é um número
-            email: document.getElementById('email').value,
-            cargo: document.getElementById('cargo').value
-        };
-
-        fetch('http://localhost:8080/servidor', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(servidor)
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao cadastrar servidor: ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            alert('Servidor cadastrado com sucesso!');
-            console.log('Success:', data);
-        })
-        .catch(error => {
-            console.error('Erro ao cadastrar servidor:', error);
-        });
-    });
+        alert('As senhas não coincidem!');
+    }
 });
